62a9b0c3ff5ffddbcce11e97122614627238fbe5
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-08-06 14:53:33 +0700

    hoàn thành crawl dữ liệu và đọc ghi file

--- src/MainCase/Main.java
+++ src/MainCase/Main.java
@@ -1,37 +1,17 @@
 package MainCase;
 
-import Data.CreateData.*;
+import Data._OIDataByTGDD.ListProducts;
 import Product.Laptop;
-import Product.Product;
 import Product.SmartPhone;
 import Product.Tablet;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        CreateListOfDataProduct listProduct = new CreateListOfDataProduct();
-        List<Product> arrayProduct = new ArrayList<>();
-
-        List<Laptop> arrayListLap = listProduct.createDataLaptop();
-        List<SmartPhone> arrayListPhone = listProduct.createDataSmartphone();
-        List<Tablet> arrayListTab = listProduct.createDataTablet();
-
-//        arrayProduct.addAll(arrayListLap);
-//        arrayProduct.addAll(arrayListPhone);
-//        arrayProduct.addAll(arrayListTab);
-
-
-        for (Laptop laptop : arrayListLap){
-            laptop.display();
-        }
-        for (SmartPhone smartPhone : arrayListPhone){
-            smartPhone.display();
-        }
-        for (Tablet tablet : arrayListTab){
-            tablet.display();
-        }
+    public static void main(String[] args) {
+        ListProducts listProducts = new ListProducts();
+        ArrayList<Laptop> listLaptop = listProducts.getListLaptop();
+        ArrayList<SmartPhone> listSmartPhone = listProducts.getListSmartphone();
+        ArrayList<Tablet> listTablet = listProducts.getListTablet();
     }
 }
